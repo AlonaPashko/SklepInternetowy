3e09602f0180a1b5d393ef43ac6124f0fc9ebea2
AlonaPashko <105510952+AlonaPashko@users.noreply.github.com>
2022-10-15 17:24:15 +0200

    Adding CoursesContext

---
+++ DAL/CoursesContext.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SklepInternetowy.DAL
+
+{
+    public class CoursesContext : DbContext
+    {
+        public CoursesContext() : base("CourseContext") 
+        {
+
+        }
+        
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem>OrderItems { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
