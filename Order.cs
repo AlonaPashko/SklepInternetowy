d12acba743ec1b00191042503366b428467b09d9
AlonaPashko <105510952+AlonaPashko@users.noreply.github.com>
2022-10-15 17:41:13 +0200

    First test database

--- Order.cs
+++ Order.cs
@@ -1,6 +1,38 @@
-﻿namespace SklepInternetowy
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SklepInternetowy
 {
-    public class Orders
+    public class Order
     {
+        public int OrderId { get; set; }
+        [Required(ErrorMessage = "Please, enter the name")]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please, enter the surname")]
+        [StringLength(50)]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Please, enter the street name")]
+        [StringLength(100)]
+        public string Street { get; set; }
+        [Required(ErrorMessage = "Please, enter the city name")]
+        [StringLength(100)]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Please, enter the ZIP Code")]
+        [StringLength(6)]
+        public string ZipCode { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+        public DateTime AddedDate { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public decimal OrderPrice { get; set; }
+
+        List<OrderItem> OrderItems { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        New,
+        Realized
     }
 }
