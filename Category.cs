5a1d9fe12dc9ad83ea33a4210a7b9fb059117482
AlonaPashko <105510952+AlonaPashko@users.noreply.github.com>
2022-10-15 16:28:21 +0200

    Adding attributes

--- Category.cs
+++ Category.cs
@@ -1,6 +1,17 @@
-﻿namespace SklepInternetowy
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SklepInternetowy
 {
     public class Category
     {
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Please, enter the name of the category")]
+        [StringLength(100)]
+        public string CategoryName { get; set; }
+        [Required(ErrorMessage = "Please, enter the description of the category")]
+        public string CategoryDescription { get; set; }
+        public string IconFileName { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
     }
 }
--- Course.cs
+++ Course.cs
@@ -1,6 +1,25 @@
-﻿namespace SklepInternetowy
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SklepInternetowy
 {
-    public class Kurs
+    public class Course
     {
+        public int CourseId { get; set; }
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Please, enter the name of the course")]
+        [StringLength(100)]
+        public string CourseName { get; set; }
+        [Required(ErrorMessage = "Please, enter the name of the autor")]
+        [StringLength(100)]
+        public string AutorOfCourse { get; set; }
+        public DateTime AddedDate { get; set; }
+        [StringLength(100)]
+        public string ImgFileName { get; set; }
+        public string CourseDescription { get; set; }
+        public decimal CoursePrice { get; set; }
+        public bool Bestseller { get; set; }
+        public bool Hidden { get; set; }
+
+        public virtual Category Category { get; set; }
     }
 }
